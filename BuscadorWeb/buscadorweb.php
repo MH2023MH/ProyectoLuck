1f227c87cb96d89596befe042ef8bfc65dd497dc
MH2023MH <manuelhvaldes@gmail.com>
2023-10-18 18:24:05 -0300

    Se arregla configuracion general del proyecto
    Se crea la carpeta styles, se meten los nuevos styles alli, se dejan apartado en una carpeta nueva (Archivos Viejos) todos los archivos viejos del proyecto, se deja funcionando todos los styles, y se agrega el logo de la plataforma

--- BuscadorWeb/buscadorweb.php
+++ BuscadorWeb/buscadorweb.php
@@ -23,9 +23,11 @@ if (isset($_SESSION['login_error'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/LinuxDataBaseWeb/styles.css">
-    <link rel="stylesheet" href="/LinuxDataBaseWeb/styles2.css">
-    <link rel="stylesheet" href="/LinuxDataBaseWeb/styles3.css">
+
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleBuscadorWeb.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleBarraDeNavegacion.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleTitle.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
--- LoginRegisterPerfil/login.php
+++ LoginRegisterPerfil/login.php
@@ -12,9 +12,11 @@ include 'C:\xampp\htdocs\LinuxDataBaseWeb\ArquitecturaGeneral\title.html'
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="\LinuxDataBaseWeb\styles1.css">
-    <link rel="stylesheet" href="\LinuxDataBaseWeb\styles2.css">
-    <link rel="stylesheet" href="\LinuxDataBaseWeb\styles3.css">
+
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleBarraDeNavegacion.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleTitle.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleLogin.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
